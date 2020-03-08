84459dcbbf3eea1c65567aa26e4e72d1410bd016
SyberHexen <SyberHexen@gmail.com>
2020-03-08 16:13:10 -0700

    sdm632-common: Update overlays with CAF tag 'LA.UM.8.6.2.r2-01600-89xx.0'
    * also bring them more inline with CAF layout
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- common.mk
+++ common.mk
@@ -38,6 +38,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-product
+
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
